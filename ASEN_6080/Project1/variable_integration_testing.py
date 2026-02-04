Fixed jacobian contribution from drag

ASEN_6080/Project1/variable_integration_testing.py
@@ -17,7 +17,7 @@ station_positions_ecef = np.array([[-5127510.0E-3, -3794160.0E-3,  0.0],
                                     [3860910.0E-3, 3238490.0E-3,  3898094.0E-3],
                                     [549505.0E-3, -1380872.0E-3,  6182197.0E-3]])  # Example ground station positions in ECEF coordinates
 
-integrator = Integrator(mu, R_e, mode=['mu','J2','Drag','Stations'], parameter_indices=[6,7,8,9], spacecraft_area=spacecraft_area, spacecraft_mass=spacecraft_mass)
+integrator = Integrator(mu, R_e, mode=['mu','J2','Drag','Stations'], parameter_indices=[6,7,8,9], number_of_stations=3, spacecraft_area=spacecraft_area, spacecraft_mass=spacecraft_mass)
 
 t_final = 3600.0 * 8 # 8 hour in seconds
 augmented_state = np.hstack((sat_state, mu, J2, C_d, station_positions_ecef.flatten()))

ASEN_6080/Tools/batch_lls_estimator.py
@@ -103,7 +103,7 @@ class BatchLLSEstimator:
                         first_station_partial_index = raw_state_length - 3 * num_stations # Assumes 3 position states per station and they are at the end of the state vector
                         station_partial_index = first_station_partial_index + i * 3
                         H_tilde[:, station_partial_index:station_partial_index+3] = H_station_tilde
-
+                    
                     H = H_tilde @ stm
                     
                     H_matrix[i, j] = H
@@ -118,7 +118,7 @@ class BatchLLSEstimator:
                     if ~np.isnan(res).any():
                         Lambda += H.T @ R_inv @ H
                         N += H.T @ R_inv @ res
-                        breakpoint()
+
 
             # Compute state correction
             x_hat = np.linalg.solve(Lambda, N)

ASEN_6080/Tools/generic_functions.py
@@ -99,6 +99,20 @@ def state_jacobian(r : np.array, V : np.array, mu : float, J2 : float, J3 : floa
 
     # Drag partials
     rho = compute_density(r_norm) * 1e9 # Convert from kg/m^3 to kg/km^3 <---- DOUBLE CHECK THIS CONVERSION
+    H = 88.6670 # Scale height in km
+
+    a_xx_drag = u * (x * rho * V_norm * C_d * spacecraft_area) / (2 * spacecraft_mass * H * r_norm)
+    a_xy_drag = u * (y * rho * V_norm * C_d * spacecraft_area) / (2 * spacecraft_mass * H * r_norm)
+    a_xz_drag = u * (z * rho * V_norm * C_d * spacecraft_area) / (2 *spacecraft_mass * H * r_norm)
+
+    a_yx_drag = v * (x * rho * V_norm * C_d * spacecraft_area) / (2 * spacecraft_mass * H * r_norm)
+    a_yy_drag = v * (y * rho * V_norm * C_d * spacecraft_area) / (2 * spacecraft_mass * H * r_norm)
+    a_yz_drag = v * (z * rho * V_norm * C_d * spacecraft_area) / (2 *spacecraft_mass * H * r_norm)
+
+    a_zx_drag = w * (x * rho * V_norm * C_d * spacecraft_area) / (2 * spacecraft_mass * H * r_norm)
+    a_zy_drag = w * (y * rho * V_norm * C_d * spacecraft_area) / (2 * spacecraft_mass * H * r_norm)
+    a_zz_drag = w * (z * rho * V_norm * C_d * spacecraft_area) / (2 * spacecraft_mass * H * r_norm)
+    
 
     a_xu_drag = -(rho * C_d * spacecraft_area) / (2*spacecraft_mass) * (u**2 / V_norm + V_norm)
     a_yv_drag = -(rho * C_d * spacecraft_area) / (2*spacecraft_mass) * (v**2 / V_norm + V_norm)
@@ -111,15 +125,17 @@ def state_jacobian(r : np.array, V : np.array, mu : float, J2 : float, J3 : floa
     a_zv_drag = a_yw_drag
 
     # Combine all partials
-    a_xx = a_xx_pm + a_xx_J2 + a_xx_J3
-    a_yy = a_yy_pm + a_yy_J2 + a_yy_J3
-    a_zz = a_zz_pm + a_zz_J2 + a_zz_J3
-    a_xy = a_xy_pm + a_xy_J2 + a_xy_J3
-    a_xz = a_xz_pm + a_xz_J2 + a_xz_J3
-    a_yz = a_yz_pm + a_yz_J2 + a_yz_J3
-    a_yx = a_xy
-    a_zx = a_xz
-    a_zy = a_yz
+    a_xx = a_xx_pm + a_xx_J2 + a_xx_J3 + a_xx_drag
+    a_xy = a_xy_pm + a_xy_J2 + a_xy_J3 + a_xy_drag
+    a_xz = a_xz_pm + a_xz_J2 + a_xz_J3 + a_xz_drag
+
+    a_yx = a_xy_pm + a_xy_J2 + a_xy_J3 + a_yx_drag
+    a_yy = a_yy_pm + a_yy_J2 + a_yy_J3 + a_yy_drag
+    a_yz = a_yz_pm + a_yz_J2 + a_yz_J3 + a_yz_drag
+
+    a_zx = a_xz_pm + a_xz_J2 + a_xz_J3 + a_zx_drag
+    a_zy = a_yz_pm + a_yz_J2 + a_yz_J3 + a_zy_drag
+    a_zz = a_zz_pm + a_zz_J2 + a_zz_J3 + a_zz_drag
 
     # Compute velocity partials (all zeros)
     vel_partials = np.zeros((3, 3))