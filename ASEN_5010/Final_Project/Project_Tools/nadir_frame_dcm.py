001f524606700aebc692fc1be4286b0ce51a85c2
Alex Willebrand <awillebrand@gmail.com>
2026-03-30 19:14:10 -0600

    Completed Task 4 for ASEN 5010 final project.

---
+++ ASEN_5010/Final_Project/Project_Tools/nadir_frame_dcm.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+from Project_Tools.hill_frame_dcm import hill_frame_dcm
+import numpy as np
+
+def nadir_frame_dcm(time):
+    """
+    This function calculates the DCM from the inertial frame to the LMO nadir frame at a given time.
+
+    Parameters
+    ----------
+    time : float
+        The time at which to calculate the DCM in seconds.
+
+    Returns
+    -------
+    DCM : numpy array
+        The direction cosine matrix from the inertial frame to the nadir frame.
+    """
+
+    # Get the DCM from inertial to Hill frame
+    DCM_hill = hill_frame_dcm(time)
+
+    # The nadir frame is defined such that the 1st axis points towards the center of the planet,
+    # which is the opposite direction of the 1st axis of the Hill frame, the 2nd axis is the same
+    # as the 2nd axis of the Hill frame, and the 3rd axis is defined to complete the right-handed frame.
+
+    R_nadir_hill = np.array([[-1, 0, 0],
+                             [0, 1, 0],
+                             [0, 0, -1]])
+
+    # Calculate the DCM from inertial to nadir frame
+    DCM_nadir = R_nadir_hill @ DCM_hill
+
+    return DCM_nadir
---
+++ ASEN_5010/Final_Project/Testing_Scripts/task_4_testing.py
@@ -0,0 +1,35 @@
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+from Project_Tools.nadir_frame_dcm import nadir_frame_dcm
+from Project_Tools.hill_frame_dcm import hill_frame_dcm
+from initial_conditions import ta_dot_LMO
+import numpy as np
+
+# Define time to test the function at
+t = 330
+
+# Test the function at the specified time
+DCM = nadir_frame_dcm(t)
+
+# Print the results
+print("DCM from inertial frame to nadir frame at t =", t, "seconds:")
+print(DCM)
+
+# Define the angular velocity of the frame relative to the inertial frame in the nadir frame
+# The angular velocity of the nadir frame is directly related to the angular velocity of the Hill frame, which is equal to the rate of change of the true anomaly (ta_dot_LMO) in this case
+omega_H = np.array([0, 0, np.deg2rad(ta_dot_LMO)])  # Assuming the nadir frame rotates about the z-axis of the Hill frame
+
+# Convert the angular velocity to the inertial frame using the DCM
+HN = hill_frame_dcm(t)  # DCM from inertial to Hill frame
+omega_inertial = HN.T @ omega_H  # Convert angular velocity to inertial frame
+
+# print the angular velocity
+print("Angular velocity of the nadir frame relative to the inertial frame (in nadir frame):", omega_inertial)
+
+# Write the result to a text file in the coursera_validation_files directory
+with open(os.path.join(os.path.dirname(__file__), '..', 'coursera_validation_files', 'nadir_frame_dcm.txt'), 'w') as f:
+    f.write(' '.join(str(x) for x in DCM.flatten()))
+
+with open(os.path.join(os.path.dirname(__file__), '..', 'coursera_validation_files', 'nadir_frame_angular_velocity.txt'), 'w') as f:
+    f.write(' '.join(str(x) for x in omega_inertial.flatten()))
