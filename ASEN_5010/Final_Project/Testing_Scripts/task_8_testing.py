5eb392b1796ed50a3a9126436d3421c643d3c755
Alex Willebrand <awillebrand@gmail.com>
2026-04-12 11:28:33 -0600

    Tasks 8-10 done (?)

--- ASEN_5010/Final_Project/Testing_Scripts/task_8_testing.py
+++ ASEN_5010/Final_Project/Testing_Scripts/task_8_testing.py
@@ -7,12 +7,15 @@ from Project_Tools.RK4 import rk4
 from Project_Tools.control_vector import compute_gains
 from Project_Tools.attitude_error_eval import attitude_error_eval
 from Project_Tools.sun_frame_funcs import sun_frame_dcm, sun_frame_angular_velocity
+from Project_Tools.nadir_frame_dcm import nadir_frame_dcm, nadir_frame_angular_velocity
+from Project_Tools.GMO_pointing_frame_funcs import GMO_pointing_frame_dcm, GMO_pointing_frame_angular_velocity
+
 from initial_conditions import sigma_0_LMO, omega_0_LMO, I_LMO
 
 # Define time array for integration
 t_0 = 0
 t_f = 400
-dt = 0.01
+dt = 1
 t = np.arange(t_0, t_f + dt, dt)
 
 # Compute control gains
@@ -23,7 +26,22 @@ P, K = compute_gains(I_LMO, decay_time_constant)
 y0 = np.hstack((sigma_0_LMO, np.deg2rad(omega_0_LMO)))
 
 # Run RK4 integration for the given initial conditions and control gains
-pointing_mode = 'Sun'
+pointing_mode = 'nadir'
+
+if pointing_mode.lower() == 'sun':
+    reference_frame_dcm_func = sun_frame_dcm
+    reference_frame_angular_velocity_func = sun_frame_angular_velocity
+    task_num = 8
+elif pointing_mode.lower() == 'nadir':
+    reference_frame_dcm_func = nadir_frame_dcm
+    reference_frame_angular_velocity_func = nadir_frame_angular_velocity
+    task_num = 9
+elif pointing_mode.lower() == 'gmo':
+    reference_frame_dcm_func = GMO_pointing_frame_dcm
+    reference_frame_angular_velocity_func = GMO_pointing_frame_angular_velocity
+    task_num = 10
+else:
+    raise ValueError("Invalid pointing mode. Must be 'GMO', 'Nadir', or 'Sun'.")
 
 solution = rk4(y0, t, I_LMO, pointing_mode, P, K)
 
@@ -39,8 +57,8 @@ attitude_errors = np.zeros((len(t), 6))  # Initialize vector to store attitude e
 for i, time in enumerate(t):
     sigma_BN = solution[i, :3]
     omega_BN = solution[i, 3:]
-    reference_frame_dcm = sun_frame_dcm(time)
-    reference_frame_angular_velocity = sun_frame_angular_velocity(time)
+    reference_frame_dcm = reference_frame_dcm_func(time)
+    reference_frame_angular_velocity = reference_frame_angular_velocity_func(time)
     sigma_error, omega_error = attitude_error_eval(time, sigma_BN, omega_BN, reference_frame_dcm, reference_frame_angular_velocity)
     attitude_errors[i, :3] = sigma_error
     attitude_errors[i, 3:] = omega_error
@@ -62,7 +80,7 @@ fig.show()
 
 # Save solutions at interest times to files
 for i, time in enumerate(times_of_interest):
-    with open(os.path.join(os.path.dirname(__file__), '..', 'coursera_validation_files', f'task_8_solution_time_{time}.txt'), 'w') as f:
+    with open(os.path.join(os.path.dirname(__file__), '..', 'coursera_validation_files', f'task_{task_num}_solution_time_{time}.txt'), 'w') as f:
         f.write(' '.join(str(x) for x in solutions_at_interest_times[i,:3]))
-with open(os.path.join(os.path.dirname(__file__), '..', 'coursera_validation_files', 'task_8_gains.txt'), 'w') as f:
+with open(os.path.join(os.path.dirname(__file__), '..', 'coursera_validation_files', f'task_{task_num}_gains.txt'), 'w') as f:
     f.write(' '.join(str(gain) for gain in [P, K]))
